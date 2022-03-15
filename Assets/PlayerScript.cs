ca9cd2719e6fa6f1fea879d794f0c2e6c1ea6cb2
Nikita Afanasev <3kokokolololo3@gmail.com>
2022-03-15 15:39:26 +0300

    Move and Collect items

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidbody;
+    public Text collectedText;
+    public static int collectedAmount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+        collectedText.text = "Items Collected: " + collectedAmount; 
+    }
+}
